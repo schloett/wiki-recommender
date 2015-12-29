7fcda5e8aa540223c638982c283958fe144b3fbc
kunzstef <kunzstef@fim.uni-passau.de>
2015-12-29 22:52:39 +0100

    markup module imported + documentation updated

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# chrome-extension
+# wiki-recommender chrome-extension
+
+This chrome extension is based on the EEXCESS [chrome-extension](https://github.com/EEXCESS/chrome-extension).
 
 ## setup
-1. Clone the repository (it might also be necessary to initalize the submodules with `git submodule update --init --recursive`)
-2. Checkout branch 'develop'
-3. Use `npm install` to install the required node modules (requires [node.js](https://nodejs.org/))
-4. Use `bower install` to install the dependencies (if bower however has not been made available by npm, try installing it manually and globally with `npm -g install bower`)
-5. Go to `chrome://extensions/`
-6. Activate developer mode
-7. Click on  `load an unpacked extension`, locate and select your cloned repo
+1. Clone the repository
+2. Use `npm install` to install the required node modules (requires [node.js](https://nodejs.org/))
+3. Use `bower install` to install the dependencies (if bower however has not been made available by npm, try installing it manually and globally with `npm -g install bower`)
+4. Go to `chrome://extensions/`
+5. Activate developer mode
+6. Click on  `load an unpacked extension`, locate and select your cloned repo
+
+## events
+
+For inserting the citations in the markup used by wikipedia (wiki-code).  
+The following message events are used to insert the citation at the current cursor position in the edited article:
+* `eexcess.insertMarkup.text`:  
+   Inserts the title of a document followed by a reference  referenced at the end of the article in the references section.  
+* `eexcess.insertMarkup.image`
--- js/markup.js
+++ js/markup.js
@@ -3,27 +3,31 @@ require(['c4/cmsMarkup'], function (cms) {
 
     var insertMarkupHandler = function (msg) {
         if (msg.data.event && msg.data.event.startsWith('eexcess.insertMarkup')) {
+            var markupText;
+
             switch (msg.data.event) {
                 case 'eexcess.insertMarkup.text':
-                    //api.sendLog(api.insertMarkup.text, msg.data.data);
-                    var insertAtCaret = function (textarea, text) {
-                        var startPos = textarea.selectionStart;
-                        var endPos = textarea.selectionEnd;
-                        var scrollTop = textarea.scrollTop;
-                        textarea.value = textarea.value.substring(0, startPos) + text + textarea.value.substring(endPos, textarea.value.length);
-                        textarea.focus();
-                        textarea.selectionStart = startPos + text.length;
-                        textarea.selectionEnd = startPos + text.length;
-                        textarea.scrollTop = scrollTop;
-                    };
-
-                    insertAtCaret($('textarea#wpTextbox1')[0], cms.createMarkup(msg.data.data.documentInformation, markup));
+                    markupText = cms.createMarkup(msg.data.data.documentInformation, markup);
                     break;
                 case 'eexcess.insertMarkup.image':
-                    //api.sendLog(api.insertMarkup.image, msg.data.data);
-                    break;
+                    break; // TODO
+            }
+
+            if (markupText) {
+                insertAtCaret($('textarea#wpTextbox1')[0], markupText);
             }
         }
+    };
+
+    var insertAtCaret = function (textarea, text) {
+        var startPos = textarea.selectionStart;
+        var endPos = textarea.selectionEnd;
+        var scrollTop = textarea.scrollTop;
+        textarea.value = textarea.value.substring(0, startPos) + text + textarea.value.substring(endPos, textarea.value.length);
+        textarea.focus();
+        textarea.selectionStart = startPos + text.length;
+        textarea.selectionEnd = startPos + text.length;
+        textarea.scrollTop = scrollTop;
     };
 
     window.addEventListener('message', insertMarkupHandler)
