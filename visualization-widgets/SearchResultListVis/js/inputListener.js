91d705ff5df3fe406bb5aecc10b6d4fbdefc0cd5
Lisa Wagner <github@lisawagner.li>
2016-01-19 18:23:09 +0100

    #4 moved wiki commons call to background.js, removed wikiRecommenderApi.js

--- visualization-widgets/SearchResultListVis/js/inputListener.js
+++ visualization-widgets/SearchResultListVis/js/inputListener.js
@@ -1,14 +1,24 @@
-/**
- * Created by lisa on 10/12/15.
- */
 
     // Prevent form submission
 $("form").submit(function (event) {
     $("div").remove(".eexcess-isotope-grid-item");
     event.preventDefault();
     var input = $("#wiki-recommender-active-search").val();
-    //console.log(input);
 
-    $(getCommonsImages(input));
 
+    //$(getCommonsImages(input));
+
+    chrome.runtime.sendMessage({
+        method: 'triggerQueryCommons', data: {
+            origin: {module: "wikiRecommender"},
+            contextKeywords: [
+                {text: input}
+            ]
+        }
+    }, function (response) {
+        //console.log(response)
+        addWikiGrid(response.data);
+
+        //window.top.postMessage({event: 'eexcess.newResults', data: response}, '*');
+    });
 });
