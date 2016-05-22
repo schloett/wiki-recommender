5a8813c302e55c6922c220d18983ffc6dcc42329
kunzstef <kunzstef@fim.uni-passau.de>
2016-05-22 09:52:22 +0200

    wiki commons provider selection added to the options menu

--- js/options.js
+++ js/options.js
@@ -5,10 +5,17 @@ require(['./common'], function(common) {
         // store current values and inform background script about update
         var update = function() {
             var selectedSources = [];
+            var queryWikiCommons = false;
             $.each($('#partnerList input:checked'), function() {
-                selectedSources.push($(this).data('props'));
+                var data = $(this).data('props');
+
+                if (data.systemId === WIKI_COMMONS.systemId) {
+                    queryWikiCommons = true;
+                } else {
+                    selectedSources.push(data);
+                }
             });
-            chrome.storage.sync.set({numResults: $numResults.val(), selectedSources: selectedSources}, function() {
+            chrome.storage.sync.set({numResults: $numResults.val(), selectedSources: selectedSources, queryWikiCommons: queryWikiCommons}, function() {
                 chrome.runtime.sendMessage({method: 'optionsUpdate'});
             });
         };
@@ -51,38 +58,53 @@ require(['./common'], function(common) {
 
         // partner list
         var $sources = $('#sources');
+        const WIKI_COMMONS = {
+            systemId: 'Wikimedia Commons',
+            favIconURI: 'https://commons.wikimedia.org/static/favicon/commons.ico'
+        };
         api.getRegisteredPartners(function(res) {
             $('#loader').hide();
             if (res.status === 'success') {
                 var $partnerList = $('<ul id="partnerList">');
+
+                // add wiki commons
+                res.data.partner.push(WIKI_COMMONS);
+
                 $.each(res.data.partner, function() {
                     var data = {
                         systemId: this.systemId,
                         favIconURI: this.favIconURI
                     };
                     var li = $('<li></li>');
-                    var input = $('<input type="checkbox" name="' + this.systemId + '" value="' + this.systemId + '" /><img src="' + this.favIconURI + '" class="partnerIcon" />').data('props', data).change(update);
+                    var input = $('<input type="checkbox" name="' + formatId(this.systemId) + '" value="' + this.systemId + '" /><img src="' + this.favIconURI + '" class="partnerIcon" />').data('props', data).change(update);
                     li.append(input).append(' ' + this.systemId);
                     $partnerList.append(li);
                 });
                 $sources.append($partnerList);
                 // get selection from storage
-                chrome.storage.sync.get('selectedSources', function(res) {
+                chrome.storage.sync.get(['selectedSources', 'queryWikiCommons'], function(res) {
                     if (res.selectedSources) {
                         res.selectedSources.forEach(function(val) {
-                            $('#partnerList input[name=' + val.systemId + ']').prop('checked', true);
+                            $('#partnerList input[name=' + formatId(val.systemId) + ']').prop('checked', true);
                         });
+                    }
+                    if (res.queryWikiCommons === true) {
+                        $('#partnerList input[name=' + formatId(WIKI_COMMONS.systemId) + ']').prop('checked', true);
                     }
                 });
             } else {
                 $sources.append('Failed to retrieve available providers, showing only currently selected');
-                chrome.storage.sync.get('selectedSources', function(res) {
-                    if (res.selectedSources && res.selectedSources.length > 0) {
+                chrome.storage.sync.get(['selectedSources', 'queryWikiCommons'], function(res) {
+                    if (res.selectedSources && (res.selectedSources.length > 0 || res.queryWikiCommons)) {
                         var $partnerList = $('<ul id="partnerList">');
+
+                        // add wiki commons
+                        if (res.queryWikiCommons)
+                            res.selectedSources.push(WIKI_COMMONS);
+
                         res.selectedSources.forEach(function(val) {
                             var li = $('<li></li>');
                             var input = $('<input type="checkbox" name="' + val.systemId + '" value="' + val.systemId + '" /><img src="' + val.favIconURI + '" class="partnerIcon" />').data('props', val).prop('checked', true).change(update);
-                            ;
                             li.append(input).append(' ' + val.systemId);
                             $partnerList.append(li);
                         });
@@ -93,3 +115,7 @@ require(['./common'], function(common) {
         });
     });
 });
+
+function formatId(string) {
+    return string.replace(/\s+/g, '-').toLowerCase();
+}
